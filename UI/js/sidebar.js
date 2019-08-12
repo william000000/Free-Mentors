87dfbd809b9bfe31be39fde9d14882467bfe0e66
William000000 <sugiraw@gmail.com>
2019-08-12 18:09:02 +0200

    ft(View Mentors): user view all mentors - View all mentors - Responsiveness of my pages [Finishes #167602972]

--- UI/js/sidebar.js
+++ UI/js/sidebar.js
@@ -1,11 +1,20 @@
 function displaySideBar(){
   document.querySelector('#sidebar').style.display = "block";
-  document.querySelector('#header-nav').style.display = "none";
+
 
 } 
 function closebtn(){
   document.querySelector('#sidebar').style.display = "none";
   document.querySelector('#btntoggle').style.display = "block";
-  document.querySelector('#header-nav').style.display = "flex";
+
 }
 
+
+function showMoreMentors(){
+  document.querySelector('.viewMore').style.display = "none";
+  document.querySelector('.moreMentor').style.display = "flex";
+}
+function lessMentors(){
+  document.querySelector('.moreMentor').style.display = "none";
+  document.querySelector('.viewMore').style.display = "block";
+}
