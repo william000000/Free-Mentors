eab53228cc48147394ff8844fc642e2086addd57
William000000 <sugiraw@gmail.com>
2019-08-20 14:52:33 +0200

    ft(api view mentor): User view specific mentor - Endpoint, User view a mentor - Test it - Validate it  when no token provided
    [Finishes #167999104]

--- src/controllers/mentors.controller.js
+++ src/controllers/mentors.controller.js
@@ -13,8 +13,29 @@ class MentorController {
 
     return res.status(200).json({
       status: 200,
-      data: { findMentors }
+      data: findMentors 
     });
   }
+
+  /**
+  * Get Specific mentor
+  * @param {object} req
+  * @param {object} res
+  */
+  static getOneMentor(req, res) {
+    const findMentor = mentors.find(mt => mt.mentorId == req.params.mentorId);
+    if (findMentor) {
+      return res.status(200).json({
+        status: 200,
+        data: findMentor 
+      });
+    }
+    return res.status(404).json({
+      status: 404,
+      error: "Not found"
+    });
+
+  }
 }
+
 export default MentorController;
